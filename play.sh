43fc59bc0a8f27540c965a742e93d8e357b74a30
juhi-121 <43788975+juhi-121@users.noreply.github.com>
2018-12-06 14:36:23 +0530

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#simple script that searches all the mp3 files in the system and plays them using mplayer(a media player for linux terminal)
 find /home/juhi121 -type f -iname "*.mp3" > playlist.m3u #Replace path by your home directory
 mplayer -playlist playlist.m3u
 
